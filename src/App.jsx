51a3f696573e2c0a9beaef45e57639ef30f561b5
krishna <krishnasinghrathore558@gmail.com>
2025-07-23 10:00:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-gray-700'>hi  i done a great  job</div>
+    </>
+  )
+}
+
+export default App
